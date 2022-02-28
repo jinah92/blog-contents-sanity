ab62302266491907d9fa91ef0d3b02299f7f7f82
jinah92 <jinah0818@gmail.com>
2022-02-28 23:11:41 +0900

    sanify sample blog

---
+++ schemas/video.js
@@ -0,0 +1,12 @@
+export default {
+  name: "video",
+  title: "Video",
+  type: "object",
+  fields: [
+    {
+      name: "caption",
+      title: "Caption",
+      type: "string",
+    },
+  ],
+};
